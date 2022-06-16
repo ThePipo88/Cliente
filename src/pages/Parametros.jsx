0f3390c6e0bcf1a5b05650bf578cba921ff91c09
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2022-06-15 20:54:36 -0600

    Parametros

--- src/pages/Parametros.jsx
+++ src/pages/Parametros.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 import Navbar from "../components/Dashboard/Navbar";
+import Parametro from "../components/Parametros/Parametro";
+import { Alert } from 'antd';
+import { useState } from "react";
 
 function Parametros() {
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleNameChange = (newName)=>{
+    setShowAlert(newName);
+  };
+
   return (
     <div className="metrics">
       <Navbar name = "Parametros"/>
       <div className="grid-one">
-
+      <Parametro nameChange={handleNameChange}/>
       </div>
     </div>
   );
