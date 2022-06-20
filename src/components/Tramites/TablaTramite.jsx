b505936d5ca3c811480b6dbc10331b620af96d5e
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2022-06-19 23:10:31 -0600

    Tramites Funcionalidad primera ventana

--- src/components/Tramites/TablaTramite.jsx
+++ src/components/Tramites/TablaTramite.jsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Table, Input, Button, Space } from 'antd';
 import {Highlighter} from 'react-highlight-words';
 import { SearchOutlined } from '@ant-design/icons';
 import {useNavigate } from 'react-router-dom';
 import 'antd/dist/antd.min.css';
+import Swal from 'sweetalert2';
+import axios from "axios";
 
 const App = () => {
   const [searchText, setSearchText] = useState('');
@@ -20,22 +22,64 @@ const App = () => {
     navigate("/admin/tramites/editartramites", {state:{myData}});
   }
 
-  const data = [
-    {
-      key: '1',
-      tramite: 'Contratacion Laboral',
-      descripcion:'Se requiere programador junior',
-      departamento:'Recursos Humanos',
-      editarTramite: <button className='button-37' onClick={() => editarTramite()} ></button>,
-    },
-    {
-      key: '2',
-      tramite: 'Concurso Limpieza',
-      descripcion:'Se requiere limpiar el lote de la oficina',
-      departamento:'Recursos Humanos',
-      editarTramite: <button className='button-37'></button>,
+
+  const [dataSource, setDataSource] = useState([
+
+  ]);
+
+  useEffect(() => {
+    return () => {
+      actualizarTablaT();
     }
-  ];
+  },[]);
+
+  async function actualizarTablaT(){
+    (async () => {
+      axios.get('http://localhost:3977/api/v1/tramites/getAll')
+      .then(({data}) => {
+
+        for(let i = 0; i < data.user.length; i++){   
+            const newStudent = {
+            key: i,
+            id: data.user[i]._id,
+            tramite: data.user[i].tipo_tra,
+            departamento: data.user[i].departamento_id,
+            descripcion: data.user[i].descripcion_tra,
+            editarTramite: <button className='button-37' onClick={() => editarTramite(newStudent.id, newStudent.departamento)}></button>,
+            };
+            setDataSource((pre) => {
+              return [...pre, newStudent];
+            });
+        }
+
+  
+      }).catch(({response}) => {
+
+  if(response.status == "500"){
+    Swal.fire({
+      title: 'Organizacion o correo ingresado ya existentes',
+      icon: 'warning',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'
+      }).then((result) => {
+      
+      })
+  }else if(response.status == "500"){
+    Swal.fire({
+      title: 'Se produjo un error',
+      icon: 'warning',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'
+      }).then((result) => {
+      
+      })
+  }
+})
+    })();
+
+  }
 
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
@@ -161,7 +205,7 @@ const App = () => {
       key: 'editarTramite',
     },
   ];
-  return <Table columns={columns} dataSource={data} />;
+  return <Table columns={columns} dataSource={dataSource} />;
 };
 
 export default App;
